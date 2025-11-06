9c326b244ad842be921678e30565c6cb6f30e774
Roshan0310 <roshanrosh0310@gmail.com>
2025-11-06 11:02:49 +0530

    Certificate section added

--- assets/assets.js
+++ assets/assets.js
@@ -1,133 +1,222 @@
-import user_image from './user-image.png';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import googleads from './googleads.png';
-import googleAnalytics from './googleAnalytics.png';
-import metaAds from './metaAds.png';
-import canva from './canva.png';
-import hubspot from './hubspot.png';
+import user_image from "./user-image.png";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile-img.png";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
+import googleads from "./googleads.png";
+import googleAnalytics from "./googleAnalytics.png";
+import metaAds from "./metaAds.png";
+import canva from "./canva.png";
+import hubspot from "./hubspot.png";
+import hubspot_certficate from "./hubspot-Academy-Certificate.jpg";
 export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    googleads,
-    googleAnalytics,
-    metaAds,
-    canva,
-    hubspot
-
+  user_image,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
+  googleads,
+  googleAnalytics,
+  metaAds,
+  canva,
+  hubspot,
+  hubspot_certficate,
 };
 
 export const workData = [
-    {
-        title: 'Certi-Verifier',
-        description: 'https://github.com/ASWINP333/certi_verifier',
-        live:'https://certiverifier-main.netlify.app/',
-        bgImage: '/work-1.png',
-    },
-    {
-        title: 'Tomato-Food_del',
-        description: 'https://github.com/ASWINP333/food_delivery',
-        live:'https://fooddeliveryclient.netlify.app/',
-        bgImage: '/work-2.png',
-    },
-    {
-        title: 'Recipe-Hub',
-        description: 'https://github.com/ASWINP333/Recipe_Hub_Assessment',
-        live:'https://recipe-hub-tg.netlify.app/',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'Kerala-Tourism',
-        description: 'https://github.com/ASWINP333/kerala-tourism',
-        live:'https://kerala-tourism1-website.netlify.app/',
-        bgImage: '/work-4.png',
-    },
-]
+  {
+    title: "Certi-Verifier",
+    description: "https://github.com/ASWINP333/certi_verifier",
+    live: "https://certiverifier-main.netlify.app/",
+    bgImage: "/work-1.png",
+  },
+  {
+    title: "Tomato-Food_del",
+    description: "https://github.com/ASWINP333/food_delivery",
+    live: "https://fooddeliveryclient.netlify.app/",
+    bgImage: "/work-2.png",
+  },
+  {
+    title: "Recipe-Hub",
+    description: "https://github.com/ASWINP333/Recipe_Hub_Assessment",
+    live: "https://recipe-hub-tg.netlify.app/",
+    bgImage: "/work-3.png",
+  },
+  {
+    title: "Kerala-Tourism",
+    description: "https://github.com/ASWINP333/kerala-tourism",
+    live: "https://kerala-tourism1-website.netlify.app/",
+    bgImage: "/work-4.png",
+  },
+];
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Web Design & Development', description: 'I craft result-driven digital marketing strategies combining SEO, social media marketing, and content optimization. My goal is to help brands improve visibility, attract qualified traffic, and generate measurable business growth.', link: '' },
-    { icon: assets.mobile_icon, title: 'Digital Marketing & SEO', description: 'I design intuitive interfaces and engaging user experiences that align with a brand’s identity and goals. From wireframes to final layouts, I ensure every design decision supports conversion and customer satisfaction.', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX Design & Brand Strategy', description: 'Web Design, Web Development, MERN Stack, SEO-friendly Websites, Digital Marketing, Social Media Marketing, UI/UX Design, Brand Strategy, Malappuram India.', link: '' },
-    { icon: assets.ui_icon, title: 'SEM & SMM', description: 'I create high-performing SEM and SMM campaigns to improve online visibility, increase website traffic, and generate quality leads through platforms like Google Ads, Instagram, and Facebook.', link: '' },
-    { icon: assets.ui_icon, title: 'Content Marketing', description: 'I develop SEO-optimized content strategies that strengthen brand identity, improve search engine rankings, and attract targeted audiences through blogs, social media posts, and engaging digital stories.', link: '' },
-    { icon: assets.ui_icon, title: 'E-mail MArketing', description: 'I design personalized email campaigns that increase customer engagement, nurture leads, and boost conversions with automation, segmentation, and compelling content that drives measurable business growth.', link: '' },
-    ]
+  {
+    icon: assets.web_icon,
+    title: "Web Design & Development",
+    description:
+      "I craft result-driven digital marketing strategies combining SEO, social media marketing, and content optimization. My goal is to help brands improve visibility, attract qualified traffic, and generate measurable business growth.",
+    link: "",
+  },
+  {
+    icon: assets.mobile_icon,
+    title: "Digital Marketing & SEO",
+    description:
+      "I design intuitive interfaces and engaging user experiences that align with a brand’s identity and goals. From wireframes to final layouts, I ensure every design decision supports conversion and customer satisfaction.",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "UI/UX Design & Brand Strategy",
+    description:
+      "Web Design, Web Development, MERN Stack, SEO-friendly Websites, Digital Marketing, Social Media Marketing, UI/UX Design, Brand Strategy, Malappuram India.",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "SEM & SMM",
+    description:
+      "I create high-performing SEM and SMM campaigns to improve online visibility, increase website traffic, and generate quality leads through platforms like Google Ads, Instagram, and Facebook.",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "Content Marketing",
+    description:
+      "I develop SEO-optimized content strategies that strengthen brand identity, improve search engine rankings, and attract targeted audiences through blogs, social media posts, and engaging digital stories.",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "E-mail MArketing",
+    description:
+      "I design personalized email campaigns that increase customer engagement, nurture leads, and boost conversions with automation, segmentation, and compelling content that drives measurable business growth.",
+    link: "",
+  },
+];
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript,React Js, Node Js,Express js, MongoDb,Wordpress' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Masters in Computer Application (MCA)' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built and deployed 5+ web and digital marketing projects' }
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description:
+      "HTML, CSS, JavaScript,React Js, Node Js,Express js, MongoDb,Wordpress",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description: "Masters in Computer Application (MCA)",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description: "Built and deployed 5+ web and digital marketing projects",
+  },
 ];
 
 export const toolsData = [
-    assets.vscode, assets.mongodb,assets.metaAds, assets.figma, assets.git,assets.googleads,assets.googleAnalytics,assets.canva,assets.hubspot
+  assets.vscode,
+  assets.mongodb,
+  assets.metaAds,
+  assets.figma,
+  assets.git,
+  assets.googleads,
+  assets.googleAnalytics,
+  assets.canva,
+  assets.hubspot,
+];
+
+export const certificatesData = [
+  {
+    title: "Content Marketing Certificate",
+    certificateIssuer: "Hubspot Academy",
+    img: assets.hubspot_certficate,
+  },
+  {
+    title: "Content Marketing Certificate",
+    certificateIssuer: "Hubspot Academy",
+    img: assets.hubspot_certficate,
+  },
+  {
+    title: "Content Marketing Certificate",
+    certificateIssuer: "Hubspot Academy",
+    img: assets.hubspot_certficate,
+  },
+  {
+    title: "Content Marketing Certificate",
+    certificateIssuer: "Hubspot Academy",
+    img: assets.hubspot_certficate,
+  },
+  {
+    title: "Content Marketing Certificate",
+    certificateIssuer: "Hubspot Academy",
+    img: assets.hubspot_certficate,
+  },
 ];
