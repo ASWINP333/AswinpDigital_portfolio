c27727fb6a7f493019c4315a5641b7f19f7ab876
aswinp333 <aswinp910@gmail.com>
2025-11-18 09:47:20 +0530

    og tag updatedd

--- app/layout.js
+++ app/layout.js
@@ -71,3 +71,17 @@ export const metadata = {
   },
 };
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth ">
+      <body
+        className={`${OutfitFont.className} ${OvoFont.className} antialiased leading-8
+        overflow-x-hidden dark:bg-darkTheme
+        dark:text-white`}
+      >
+        <StructuredData />
+        {children}
+      </body>
+    </html>
+  );
+}
